d3f6eaa6e6d3b6c9f602bf9998525011272ca198
sujan pal <sujanpal79@gmail.com>
2024-11-08 16:51:05 +0530

    Update Routing on Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Sujan</h1>
-    </div>
+    <main>
+      <div>
+        <Header/>
+          <div className='pt-16'>
+            <Outlet/>
+          </div>
+        <Footer/>
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='fixed top-0 w-full h-16 bg-neutral-600 bg-opacity-75'>
+        <div className="container mx-auto px-2 bg-black">
+            <p>Header</p>
+        </div>
+    </div>
+  )
+}
+
+export default Header
