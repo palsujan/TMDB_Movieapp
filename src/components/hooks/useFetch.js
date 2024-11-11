3932a69dca1cf10e1af681b8e59001979ed5c50e
sujan pal <sujanpal79@gmail.com>
2024-11-11 22:54:16 +0530

    Update Custom hooks useFetch

---
+++ src/components/hooks/useFetch.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+
+const useFetch = (endpoint) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const fetchData = async () =>{
+    try{
+        setLoading(true);
+        const response = await axios.get(endpoint);
+        setLoading(false);
+        setData(response?.data?.results);
+
+    } catch(error){
+        console.log('error', error);
+    }
+}
+useEffect(()=>{
+    fetchData();
+},[]);
+  return {data, loading}
+}
+
+export default useFetch;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import BannerHome from '../components/BannerHome'
 import { useSelector } from 'react-redux'
 import Card from '../components/Card';
 import HorizontalScrollCard from '../components/HorizontalScrollCard';
+import axios from 'axios';
+import useFetch from '../components/hooks/useFetch';
 
 const Home = () => {
     const trendingMovies = useSelector(state => state.movies.bannerData);
     // console.log("trendingMovies",trendingMovies);
+    // const [nowPlayingData, setNowPlayingData] = useState([]);
+    const {data: nowPlayingData} = useFetch('/movie/now_playing');
+    // const fetchPlayingData = async () =>{
+    //     try{
+    //         const response = await axios.get("/movie/now_playing")
+    //         setNowPlayingData(response?.data?.results);
+
+    //     } catch(error){
+
+    //     }
+    // }
+    // useEffect(()=>{
+    //     fetchPlayingData();
+    // },[]);
   return (
     <div>
         <BannerHome/>
-        <HorizontalScrollCard data ={trendingMovies} heading = {"Trending"} />
+        <HorizontalScrollCard data ={trendingMovies} heading = {"Trending"} trending={"Trending"}/>
+        <HorizontalScrollCard data ={nowPlayingData} heading = {"Now Playing"} />
 
     </div>
   )
